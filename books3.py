a8c28cc93d4d8fc874c4d27417cb253bff4d4eed
mishuptm <ionutserban.petrescu@gmail.com>
2020-02-22 18:40:14 +0000

    Final book3

--- books3.py
+++ books3.py
@@ -15,18 +15,22 @@ class Library:
     book_scores = []
     days_left = 0
     global_books = set()
+    scanned_books = set()
     list_of_libraries = []
-    list_of_signed_up = ()
+    list_of_signed_up = []
+    required_books = set()
 
-    def __init__(self, nb_of_books, nb_of_days_for_signup, shipment_speed, list_of_books, id):
+    def __init__(self, nb_of_days_for_signup, shipment_speed, list_of_books, id):
         self.id = id
-        self.nb_of_books = nb_of_books
         self.days_to_signup = nb_of_days_for_signup
         self.speed = shipment_speed
-        self.list_of_books = list_of_books
+        self.list_of_books = set(list_of_books)
         self.sorted_books = []
         self.books_to_scan = []
         self.total_score = 0
+
+    def calculate_total(self):
+        self.total_score = self.calculate_reward(self.list_of_books)
 
     def get_sorted_books(self):
         sorted_books = []
@@ -39,28 +43,100 @@ class Library:
             output.append(item[1])
         return output
 
+    @classmethod
+    def reset(cls):
+        cls.book_scores = []
+        cls.days_left = 0
+        cls.scanned_books = set()
+        cls.list_of_libraries = []
+        cls.list_of_signed_up = []
+
+    @classmethod
+    def get_score(cls):
+        """
+        Must call this method after processing all libraries in the file and after writing to file
+        :return: int score achieved for the file
+        """
+        total = 0
+        for book in cls.scanned_books:
+            total += cls.book_scores[book]
+
+        return total
+
     def get_time_required(self):
         # print(f"{self.speed}, {len(self.list_of_books)}")
         shipments_required = len(self.list_of_books) / self.speed
         return self.days_to_signup + shipments_required
 
-    def enroll(self, set_books):
-        Library.days_left -= self.days_to_signup
-        Library.global_books.union(set_books)
-        self.books_to_scan = list(set_books)
+    def enroll(self, set_books=None):
+        """
+        This function enrolls the library in the scanning process and adds the books to the scanning list
 
-    def max_reward(self):
-        if len(self.sorted_books) == 0:
+        """
+        if self in Library.list_of_signed_up:
+            return  # Making sure the library was not already enrolled
+
+        self.sort_books()
+        if set_books is None and len(self.sorted_books) > 0:
+            if self.days_to_signup < Library.days_left:     # Checking if enough days left to enroll
+                Library.days_left -= self.days_to_signup
+                spare_days = Library.days_left
+                for day in range(Library.days_left):
+                    for shipment in range(self.speed):
+                        if len(self.sorted_books) > 0:
+                            current_book = self.sorted_books.pop()
+                            while current_book in Library.scanned_books:
+                                if len(self.sorted_books) > 0:
+                                    current_book = self.sorted_books.pop()
+                                    # print("Book exists already")
+                                else:
+                                    break
+                            Library.scanned_books.add(current_book)
+                            self.books_to_scan.append(current_book)
+                            self.total_score -= Library.book_scores[current_book]
+                            self.list_of_books.remove(current_book)
+                            # Library.book_scores[current_book] = 0
+                        else:
+                            break
+                    if len(self.sorted_books) == 0:
+                        break
+                    spare_days -= 1     # This is to see how many days there are left after enrolling the library
+
+            # self.days_to_signup = Library.days_left + 1    # This will make sure we will not add the library again
+            # Library.list_of_libraries.remove(self)
+            # print(f"Spare days {spare_days}")
+            Library.list_of_signed_up.append(self)
+        elif set_books is not None and len(set_books) > 0:
+            Library.scanned_books.union(set_books)
+            self.books_to_scan = list(set_books)
+            Library.list_of_signed_up.append(self)
+
+    def sort_books(self):
+        """
+        Sorts the books according to score
+        :return:
+        """
+        if len(self.sorted_books) == 0: # and self not in Library.list_of_signed_up:
             self.sorted_books = self.get_sorted_books()
+
+
+    def simulate_enroll(self):
+        """
+        This method simulates the enrollment of a library
+        It is good to keep track of which libraries are worth to enroll
+        :return:
+        (int: score rewarded, set: books to enroll, int days_to_spare)
+        """
+        self.sort_books()
         temp_set = set()
-        day_copy = Library.days_left
-        day_copy -= self.days_to_signup
+        spare_days = Library.days_left
+        spare_days -= self.days_to_signup
         ordered_books = self.sorted_books.copy()
-        for day in range(day_copy):
+        for day in range(spare_days):
             for shipment in range(self.speed):
                 if len(ordered_books) > 0:
                     current_book = ordered_books.pop()
-                    while current_book in Library.global_books:
+                    while current_book in Library.scanned_books:
                         if len(ordered_books) > 0:
                             current_book = ordered_books.pop()
                         else:
@@ -70,36 +146,43 @@ class Library:
                     break
             if len(ordered_books) == 0:
                 break
+            spare_days -= 1
+        return self.calculate_reward(temp_set), temp_set, spare_days
 
-
-        return self.calculate_reward(temp_set), temp_set
-
-    def calculate_reward(self, id_set):
+    def calculate_reward(self, id_set=None):
+        if id_set is None:
+            id_set = Library.scanned_books
         total = 0
         for book in id_set:
             total += Library.book_scores[book]
         return total
 
     def __str__(self):
-        return f"Nb of books: {self.nb_of_books} | " \
-               f"Days to signup: {self.days_to_signup} | " \
+        return f"Days to signup: {self.days_to_signup} | " \
                f"Books shipped per day: {self.speed} | " \
                f"Library id: {self.id} | " \
-               f"NB of books: {self.nb_of_books} | " \
+               f"NB of books: {len(self.list_of_books)} | " \
                f"Sorted books: {self.sorted_books}"
 
     @property
-    def sort(self):
-        average = self.total_score / len(self.list_of_books)
-        # print(average)
-        return (self.days_to_signup / self.speed) / self.total_score
-        # return self.total_score
+    def average(self):
+        return self.total_score / len(self.list_of_books)
 
+    @property
+    def sort(self):
+        if self.total_score < 1:
+            return 0
+        return (self.days_to_signup / self.speed) / self.total_score
+
+    @property
+    def test_sort(self):
+        # return self.calculate_reward(Library.scanned_books.difference(set(self.list_of_books)))
+        return self.calculate_reward(self.list_of_books)
 
 def read_file(file):
+    Library.reset()
     with open(file, "r") as f:
         lines = f.read().split("\n")
-        books_nb = int(lines[0].split()[0])
         library_nb = int(lines[0].split()[1])
         days_to_ship = int(lines[0].split()[2])
         books_score = {}
@@ -107,21 +190,24 @@ def read_file(file):
         for score in lines[1].split():
             books_score[key] = int(score)
             key += 1
-
+        Library.book_scores = books_score
         index = 2
         list_of_library = []
 
         for i in range(library_nb):
             params = lines[index].split()
-            library = Library(int(params[0]), int(params[1]), int(params[2]), [], i)
+            library = Library(int(params[1]), int(params[2]), [], i)
             for id in lines[index+1].split():
                 int_id = int(id)
                 library.total_score += int_id
-                library.list_of_books.append(int_id)
+                library.list_of_books.add(int_id)
+
             list_of_library.append(library)
+            library.calculate_total()
             index += 2
-    print(f"{time.time()} {file} read")
-    return books_score, days_to_ship, list_of_library
+    Library.scanned_books = set()
+    Library.days_left = days_to_ship
+    Library.list_of_libraries = list_of_library
 
 
 def write_file(file, list_of_signed_up):
@@ -137,74 +223,60 @@ def write_file(file, list_of_signed_up):
 
     with open(file.replace("txt", "out3"), "w")as f:
         f.write(output)
-    print(f"{time.time()} {file} processed")
 
+
+def sort_libraries(file):
+    """
+    This method sorts the libraries using different formulas for different files, found by trial and error
+    :param file:
+    :return:
+    """
+    if "b_" in file:
+        Library.list_of_libraries.sort(key=lambda c: c.days_to_signup / c.speed, reverse=False)
+    elif "d_" in file:
+        Library.list_of_libraries.sort(key=lambda c: c.simulate_enroll()[0], reverse=True)
+    else:
+        Library.list_of_libraries.sort(key=lambda c: c.sort, reverse=False)
+
+
+def loop_libraries(stop_days):
+    for library in Library.list_of_libraries:
+        if library.simulate_enroll()[0] > 100:
+            library.enroll()
+        elif "a_" in file:
+            library.enroll()
+        if Library.days_left < stop_days:
+            break
 
 def process(file):
-    Library.book_scores, Library.days_left, Library.list_of_libraries = read_file(file)
-    Library.list_of_libraries.sort(key=lambda c: c.sort, reverse=False)  # Sorting libraries to start with better ones
-    # print(f"{file} sorted")
-    Library.global_books = set()
-    list_of_signed_up = []
-    for library_obj in Library.list_of_libraries:
-        if Library.days_left > library_obj.days_to_signup:
-            list_of_signed_up.append(library_obj)
-            Library.days_left -= library_obj.days_to_signup
-            ordered_books = library_obj.get_sorted_books()
-            for i in range(Library.days_left):  # Performing shipments for as many days are left
-                for x in range(library_obj.speed):  # Adding x books in every shipment
-                    if len(ordered_books) > 0:
-                        current_book = ordered_books.pop()
-                        while current_book in Library.global_books:
-                            if len(ordered_books) > 0:
-                                current_book = ordered_books.pop()
-                            else:
-                                break
-                        Library.global_books.add(current_book)
-                        library_obj.books_to_scan.append(current_book)
-                    else:
-                        break
-                if len(ordered_books) == 0:
-                    break
-        # if file == "b_read_on.txt":
-            # print(f"Days left: {Library.days_left}\t"
-            #       f"Current library {library_obj.days_to_signup}\t"
-            #       f"Time required for entire library {library_obj.get_time_required()}")
-            # pass
-    # print(f"{time.time()} finished computing{file}")
-    write_file(file, list_of_signed_up)
+    tic = time.perf_counter()
+    read_file(file)
+    sort_libraries(file)
+    initial_time = Library.days_left
+    if "d_" in file:
+        for i in range(2, 17):
+            sort_libraries(file)
+            loop_libraries(initial_time / i)
+        # Library.list_of_libraries.sort(key=lambda c: c.test_sort, reverse=False)
+        loop_libraries(0)
+    else:
+        loop_libraries(initial_time / 6)
+        sort_libraries(file)
+        loop_libraries(0)
 
-
-def brute_force(file):
-    Library.book_scores, Library.days_left, Library.list_of_libraries = read_file(file)
-    Library.list_of_libraries.sort(key=lambda c: c.sort, reverse=False)
-    Library.global_books = set()
-    list_of_signed_up = []
-    # print(Library.list_of_libraries[0])
-    for i in range(len(Library.list_of_libraries)):
-        individual_score = []
-        for library in Library.list_of_libraries:
-            individual_score.append(library.max_reward())
-        max_index = individual_score.index(max(individual_score))
-        best = Library.list_of_libraries[max_index]
-        best.enroll(individual_score[max_index][1])
-        list_of_signed_up.append(best)
-        Library.list_of_libraries.remove(best)
-
-    write_file(file, list_of_signed_up)
+    write_file(file, Library.list_of_signed_up)
+    score = Library.get_score()
+    toc = time.perf_counter()
+    print(f"Processed {file} in {toc - tic}. Score = {score}")
 
 
 if __name__ == "__main__":
+
+    from test import main as test_solutions
     tic = time.perf_counter()
-    from test import main as t
-    from test import test_file
-    # for file in file_names:
-    #     process(file)
-    # process("test.txt")
-    # test_file("test.out3")
-    a = "d_tough_choices.txt"
-    brute_force(a)
+    for file in file_names:
+        process(file)
+
     toc = time.perf_counter()
-    print(f"Finished in {toc-tic}")
-    test_file(a.replace("txt", "out3"))
-    # t("out3")
+    print(f"Finished in {toc - tic}")
+    test_solutions("out3")
